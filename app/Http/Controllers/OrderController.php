d8907ec2d16e72543324f5fe6c481a9b46fe1246
Islam Bilalov <bilalov1504@gmail.com>
2023-10-16 20:24:06 +0300

    feat: payment checkout route

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Services\Orders\Models\Order;
+use App\Services\Payments\Enums\PaymentStatusEnum;
+use App\Services\Payments\Models\Payment;
+use App\Services\Payments\PaymentService;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
@@ -18,5 +23,14 @@ class OrderController extends Controller
     public function show(Order $order): View
     {
         return view('orders.show', compact('order'));
+    }
+
+    public function payment(Order $order, PaymentService $service): RedirectResponse
+    {
+        $payment = $service
+            ->createPayment($order)
+            ->handle();
+
+        return to_route('payments.checkout', $payment->uuid);
     }
 }
