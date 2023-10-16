783e0a2d9884d2be811307069e24288900036a02
Islam Bilalov <bilalov1504@gmail.com>
2023-10-16 20:24:17 +0300

    feat: payment model

--- app/Services/Payments/Models/Payment.php
+++ app/Services/Payments/Models/Payment.php
@@ -6,10 +6,13 @@ use App\Services\Payments\Enums\PaymentDriverEnum;
 use App\Services\Payments\Enums\PaymentStatusEnum;
 use App\Supports\AmountValue;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
 
 class Payment extends Model
 {
     protected $fillable = [
+        'uuid',
         'status',
         'currency_id', 'amount',
         'payable_id', 'payable_type',
@@ -22,4 +25,9 @@ class Payment extends Model
         'amount' => AmountValue::class,
         'driver' => PaymentDriverEnum::class,
     ];
+
+    public function payable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
