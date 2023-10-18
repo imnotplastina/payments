800f9d56d5198edce6f482ad92fedc7f957358a4
Islam Bilalov <bilalov1504@gmail.com>
2023-10-18 21:45:43 +0300

    feat: selected payment method

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,13 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Payment\UpdatePaymentRequest;
 use App\Services\Payments\Models\Payment;
-use Illuminate\Http\Request;
+use App\Services\Payments\Models\PaymentMethod;
+use App\Services\Payments\PaymentService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
 
 class PaymentController extends Controller
 {
-    public function checkout(Payment $payment)
+    public function checkout(Payment $payment): View
     {
-        return view('payments.checkout', compact('payment'));
+        $methods = PaymentMethod::query()
+            ->where('active', true)
+            ->get();
+
+        return view('payments.checkout', compact('payment', 'methods'));
+    }
+
+    public function method(UpdatePaymentRequest $request,
+                           PaymentService $service,
+                           Payment $payment): RedirectResponse
+    {
+        $method = $service
+            ->findPaymentMethod(($request->validated())['method_id'])
+            ->handle();
+
+        $payment = $service
+            ->updatePaymentMethod($method, $payment)
+            ->handle();
+
+        return redirect()->route('payments.process', compact('payment'));
+    }
+
+    public function process(Payment $payment): string
+    {
+        return 'Выбранный способ оплаты: ' . $payment->method->name;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/orders')->name('home');
+Route::redirect('/', '/orders')
+    ->name('home');
 
-Route::get('orders', [OrderController::class, 'index'])->name('orders');
-Route::get('orders/{order:uuid}', [OrderController::class, 'show'])->name('orders.show');
-Route::post('orders/{order:uuid}/payment', [OrderController::class, 'payment'])->name('orders.payment');
+Route::get('orders', [OrderController::class, 'index'])
+    ->name('orders');
 
-Route::get('payments/{payment:uuid}', [PaymentController::class, 'checkout'])->name('payments.checkout');
+Route::get('orders/{order:uuid}', [OrderController::class, 'show'])
+    ->name('orders.show');
+
+Route::post('orders/{order:uuid}/payment', [OrderController::class, 'payment'])
+    ->name('orders.payment');
+
+Route::get('payments/{payment:uuid}/checkout', [PaymentController::class, 'checkout'])
+    ->name('payments.checkout');
+
+Route::post('payments/{payment:uuid}/method', [PaymentController::class, 'method'])
+    ->name('payments.method');
+
+Route::get('payments/{payment:uuid}/process', [PaymentController::class, 'process'])
+    ->name('payments.process');
 
 
