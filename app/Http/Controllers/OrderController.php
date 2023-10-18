d116af73b079f92be73d969c66f97a23716b6b51
Islam Bilalov <bilalov1504@gmail.com>
2023-10-18 21:50:54 +0300

    refactor: use service

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Services\Orders\Models\Order;
-use App\Services\Payments\Enums\PaymentStatusEnum;
-use App\Services\Payments\Models\Payment;
 use App\Services\Payments\PaymentService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
