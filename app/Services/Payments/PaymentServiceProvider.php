33fda17d3e5e0fd58610cf534ef8ac5e2183f9b9
Islam Bilalov <bilalov1504@gmail.com>
2023-10-24 13:10:13 +0300

    feat: vendor view publish

--- app/Services/Payments/PaymentServiceProvider.php
+++ app/Services/Payments/PaymentServiceProvider.php
@@ -20,6 +20,12 @@ class PaymentServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->loadViewsFrom(__DIR__ . '/Views', 'payments');
+
+        $this->publishes([
+            __DIR__ . '/Views' => resource_path('views/vendor/payments')
+        ], 'payments');
+
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__ . '/Migrations');
 
