756c8b009edf48e6799c3c6433fc0f46caffca35
Islam Bilalov <bilalov1504@gmail.com>
2023-10-29 18:16:19 +0300

    feat: add new statuses

--- app/Services/Tinkoff/Enums/TinkoffPaymentStatusEnum.php
+++ app/Services/Tinkoff/Enums/TinkoffPaymentStatusEnum.php
@@ -4,5 +4,10 @@ namespace App\Services\Tinkoff\Enums;
 
 enum TinkoffPaymentStatusEnum: string
 {
+    case AUTHORIZED = 'AUTHORIZED';
+    case CONFIRMED = 'CONFIRMED';
+    case REJECTED = 'REJECTED';
+    case REVERSED = 'REVERSED';
+    case REFUNDED = 'REFUNDED';
     case NEW = 'NEW';
 }
