e8da6671b4ae2f38bc8586214e092c1e7ae512e0
Islam Bilalov <bilalov1504@gmail.com>
2023-11-05 20:03:38 +0300

    fix: terminal key value fix

--- app/Services/Tinkoff/Actions/GenerateTokenAction.php
+++ app/Services/Tinkoff/Actions/GenerateTokenAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Tinkoff\Actions;
 
+use App\Services\Tinkoff\DTOs\CreatePaymentData;
 use App\Services\Tinkoff\TinkoffConfig;
 
 final class GenerateTokenAction
@@ -18,16 +19,38 @@ final class GenerateTokenAction
 
     public function handle(array $data): string
     {
-        unset($data['Token']);
+        unset(
+            $data['Token'],
+            $data['successUrl'],
+            $data['failureUrl'],
+            $data['callbackUrl']
+        );
 
         if (isset($data['Success'])) {
             $data['Success'] = $data['Success'] ? 'true' : 'false';
         }
 
+        $data['TerminalKey'] = empty($data['TerminalKey'])
+            ? $this->config->terminal
+            : $data['TerminalKey'];
+
         $data['Password'] = $this->config->password;
+
+        $data = $this->switchToUpperCase($data);
 
         return hash('sha256', collect($data)
             ->sortKeys()
             ->implode(''));
+    }
+
+    private function switchToUpperCase(array $data): array
+    {
+        $switchedData = [];
+
+        foreach ($data as $key => $value) {
+            $switchedData[ucfirst($key)] = $value;
+        }
+
+        return $switchedData;
     }
 }
