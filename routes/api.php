f5394e17f8b6374f768fe176bd5429ab52db93d5
Islam Bilalov <bilalov1504@gmail.com>
2023-11-14 19:52:07 +0300

    feat: payment tinkoff callback api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Payments\Callbacks\TinkoffController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('payments/callback/tinkoff', TinkoffController::class)
+    ->name('payments.callback.tinkoff');
