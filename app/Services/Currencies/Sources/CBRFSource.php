29d653c7ce0bb897514b0dbc3f5c6aa0c60f6768
Islam Bilalov <bilalov1504@gmail.com>
2023-11-17 18:06:08 +0300

    feat: cbrf fetching currencies

--- app/Services/Currencies/Sources/CBRFSource.php
+++ app/Services/Currencies/Sources/CBRFSource.php
@@ -2,14 +2,35 @@
 
 namespace App\Services\Currencies\Sources;
 
+use App\Services\Currencies\DTOs\SourcePrice;
 use App\Services\Currencies\Enums\SourceEnum;
+use App\Supports\AmountValue;
 use Illuminate\Database\Eloquent\Collection;
 
 class CBRFSource extends Source
 {
     public function getPrices(): Collection
     {
-        return new Collection([1, 2 ,3]);
+        $response = json_decode(
+            json_encode(
+                simplexml_load_string(
+                    file_get_contents('https://www.cbr.ru/scripts/XML_daily.asp')
+                )
+            )
+        );
+
+        $prices = new Collection([]);
+
+        foreach ($response->Valute as $valuteData) {
+            $prices->push(new SourcePrice(
+                currency: $valuteData->CharCode,
+                value: new AmountValue(
+                    str_replace(',', '.', $valuteData->VunitRate)
+                )
+            ));
+        }
+
+        return $prices;
     }
 
     public function enum(): SourceEnum
