d9e2f47b52fb7262216a35a24e7316c07c5c3008
Islam Bilalov <bilalov1504@gmail.com>
2023-11-17 18:13:46 +0300

    feat: add casts mutators

--- app/Services/Currencies/Models/Currency.php
+++ app/Services/Currencies/Models/Currency.php
@@ -2,16 +2,25 @@
 
 namespace App\Services\Currencies\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Services\Currencies\Enums\SourceEnum;
+use App\Supports\AmountValue;
 use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
     protected $keyType = 'int';
-
     public $incrementing = false;
+
+    public const RUB = 'RUB';
+    public const USD = 'USD';
 
     protected $fillable = [
         'id', 'name',
+        'price', 'source',
+    ];
+
+    protected $casts = [
+        'price' => AmountValue::class,
+        'source' => SourceEnum::class,
     ];
 }
